4b0d48cbdf76f4515cc7416cf805e3515517907b
Nuhu Sulemana <turosung@gmail.com>
2023-09-06 08:41:16 +0100

    Add button and it changes image when tapped

--- MagicCrystalBall/MagicCrystalBall/ViewController.swift
+++ MagicCrystalBall/MagicCrystalBall/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var ballImageView: UIImageView!
 
+    let ballArray = [#imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball5")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askButtonTapped(_ sender: UIButton) {
+        
+        ballImageView.image = ballArray[Int.random(in: 0...4)]
+        
+        print("Ask tapped")
+    }
+    
 }
 
