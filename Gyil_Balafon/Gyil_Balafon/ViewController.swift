b9ee22a2b29af2ac2aa761cd88c8ad529ed53ac9
Nuhu Sulemana <turosung@gmail.com>
2023-09-10 22:56:33 +0100

    initial commit for gyil project

---
+++ Gyil_Balafon/Gyil_Balafon/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Gyil_Balafon
+//
+//  Created by Nuhu Sulemana on 10/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print("C key pressed")
+    }
+    
+}
+
