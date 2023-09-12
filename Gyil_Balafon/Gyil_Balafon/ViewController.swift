3877f1ba65e086b323be006ad7b6b7715a548192
Nuhu Sulemana <turosung@gmail.com>
2023-09-12 08:56:23 +0100

    connected sound files to keys and sound works and key tapped

--- Gyil_Balafon/Gyil_Balafon/ViewController.swift
+++ Gyil_Balafon/Gyil_Balafon/ViewController.swift
@@ -6,18 +6,26 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.titleLabel!.text! )
         
-        print("C key pressed")
     }
     
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.wav.rawValue)
+        player?.play()
+    }
 }
 
