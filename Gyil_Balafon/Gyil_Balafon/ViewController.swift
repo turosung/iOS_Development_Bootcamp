e743c221d87a759bde2876dda90a1559bc6f74ae
Nuhu Sulemana <turosung@gmail.com>
2023-09-13 23:41:37 +0100

    add animation with timer to change opacity when keys are pressed

--- Gyil_Balafon/Gyil_Balafon/ViewController.swift
+++ Gyil_Balafon/Gyil_Balafon/ViewController.swift
@@ -19,6 +19,14 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton) {
         playSound(soundName: sender.titleLabel!.text! )
+        UIView.animate(withDuration: 0.2) {
+            sender.alpha = 0.5
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            UIView.animate(withDuration: 0.2) {
+                sender.alpha = 1
+            }
+        }
         
     }
     
