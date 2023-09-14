6f5447b2a82757d633ca6fb9a8d3d1868cb99bbb
Nuhu Sulemana <turosung@gmail.com>
2023-09-14 22:03:33 +0100

    starting of project

---
+++ IsMyEggReady/IsMyEggReady/IsMyEggReady/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IsMyEggReady
+//
+//  Created by Nuhu Sulemana on 14/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
