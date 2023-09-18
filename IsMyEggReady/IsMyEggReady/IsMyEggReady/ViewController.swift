fef8587600dde4d5735efc90047afb83f7d6393e
Nuhu Sulemana <turosung@gmail.com>
2023-09-18 23:04:44 +0100

    setup countdown timer to trigger when eggs tapped

--- IsMyEggReady/IsMyEggReady/IsMyEggReady/ViewController.swift
+++ IsMyEggReady/IsMyEggReady/IsMyEggReady/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    var countDownTimer = Timer()
+    var counter = 60 
+
+    @IBAction func selectHardness(_ sender: UIButton) {
+        
+        let hardness = sender.titleLabel!.text!
+        
+        let result = eggTimes[hardness]!
+        print(result)
+        
+        startTimer()
+        print("startTimer()")
     }
-
-
+    
+    func startTimer() {
+        countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateCounter() {
+        if self.counter > 0 {
+            print("\(self.counter) seconds.")
+            counter -= 1
+        } else if self.counter == 0 {
+            countDownTimer.invalidate()
+        }
+    }
 }
 
