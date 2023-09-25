a3124281f53a3ed01e83a2a6486ce7794612fdf1
Nuhu Sulemana <turosung@gmail.com>
2023-09-25 22:32:07 +0100

    Egg timer project completed

--- IsMyEggReady/IsMyEggReady/IsMyEggReady/ViewController.swift
+++ IsMyEggReady/IsMyEggReady/IsMyEggReady/ViewController.swift
@@ -6,36 +6,60 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressIndicator: UIProgressView!
+    var player: AVAudioPlayer?
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var countDownTimer = Timer()
-    var counter = 60 
-
+    var totalTime = 0
+    var secondsElapsed = 0
+    
     @IBAction func selectHardness(_ sender: UIButton) {
+        // invalidate() stops the timer from ever firing again and requests its removal from run loop
+        countDownTimer.invalidate()
         
-        let hardness = sender.titleLabel!.text!
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
         
-        let result = eggTimes[hardness]!
-        print(result)
+        progressIndicator.progress = 0.0
+        secondsElapsed = 0
+        titleLabel.text = hardness
         
         startTimer()
-        print("startTimer()")
     }
     
     func startTimer() {
-        countDownTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        countDownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     @objc func updateCounter() {
-        if self.counter > 0 {
-            print("\(self.counter) seconds.")
-            counter -= 1
-        } else if self.counter == 0 {
+        if secondsElapsed < totalTime {
+            secondsElapsed += 1
+            progressIndicator.progress = Float(secondsElapsed) / Float(totalTime)
+        } else {
             countDownTimer.invalidate()
+            titleLabel.text = "Your egg is Ready 😋!!"
+            playSound()
         }
     }
-}
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else {
+            return
+        }
+        do {
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            player!.play()
+            } catch let error {
+                print("error: \(error)")
+            }
+        }
+    }
+
 
