489167d43a3df6d9151a80306c9962527aad8f62
Nuhu Sulemana <turosung@gmail.com>
2023-09-28 00:59:46 +0100

    created the layout for quiz game

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        questionLabel.text = "Four + Two is equal to Six"
     }
-
+    
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
