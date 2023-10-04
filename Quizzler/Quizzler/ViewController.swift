89cbd7b9f9aaf5585a4f784edbc9d73702f6b976
Nuhu Sulemana <turosung@gmail.com>
2023-10-05 00:23:54 +0100

    set up initial code and interface for quiz app

--- Quizzler/Quizzler/ViewController.swift
+++ Quizzler/Quizzler/ViewController.swift
@@ -14,16 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    let quiz = [
+        ["Four + Two is equal to Six.", "True"],
+        ["Five - Three is greater than One.", "True"],
+        ["Three + Eight is less than Ten", "False"]
+    ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        questionLabel.text = "Four + Two is equal to Six"
+        updateUI()
     }
     
     
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("That's Correct!")
+        } else {
+            print("That's Wrong !")
+        }
+        
+        if questionNumber + 1 < quiz.count {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
     }
     
 
