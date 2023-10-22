d160f6359d66cf3bab0cbd1f5d11605afb887971
Nuhu Sulemana <turosung@gmail.com>
2023-10-22 05:07:55 +0100

    connected height and weight sliders to viewcontroller

--- BMI_Calculator/BMI_Calculator/Controllers/ViewController.swift
+++ BMI_Calculator/BMI_Calculator/Controllers/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+      
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSlideChanged(_ sender: UISlider) {
+        let height = (String(format: "%.2f", sender.value))
+        heightLabel.text = "\(height) m"
+    }
+    
+    @IBAction func weightSlideChanged(_ sender: UISlider) {
+        let weight = (String(format: "%.2f", sender.value))
+        weightLabel.text = "\(weight) Kg"
+    }
+  
 }
 
