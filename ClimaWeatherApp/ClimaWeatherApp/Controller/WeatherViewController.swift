901c968837c438d8258b1dc5109ddebd66f06055
Nuhu Sulemana <turosung@gmail.com>
2023-11-08 00:55:47 +0000

    implemented textfield for location search

--- ClimaWeatherApp/ClimaWeatherApp/Controller/WeatherViewController.swift
+++ ClimaWeatherApp/ClimaWeatherApp/Controller/WeatherViewController.swift
@@ -1,24 +1,50 @@
 //
 //  ViewController.swift
-//  Clima
+//  ClimaWeatherApp
 //
-//  Created by Angela Yu on 01/09/2019.
-//  Copyright © 2019 App Brewery. All rights reserved.
+//  Created by Nuhu Sulemana on 02/11/2023.
+//
 //
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type Something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        // Use searchTextField.text to get the weather for that city.
+        searchTextField.text = ""
+    }
+    
 }
 
