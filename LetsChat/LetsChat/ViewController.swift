915f13ec8113032c3ae0e74c7835274cf6c6ca9f
Nuhu Sulemana <turosung@gmail.com>
2024-01-03 07:48:39 +0000

    initial project commit

---
+++ LetsChat/LetsChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsChat
+//
+//  Created by Nuhu Sulemana on 03/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
