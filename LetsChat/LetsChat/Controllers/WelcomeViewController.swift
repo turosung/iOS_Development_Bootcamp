cb764125d50c0b4896f92ef13e8e309872bf28ea
Nuhu Sulemana <turosung@gmail.com>
2024-01-30 14:12:33 +0000

    Added cocoapods dependency and firebase to project

--- LetsChat/LetsChat/Controllers/WelcomeViewController.swift
+++ LetsChat/LetsChat/Controllers/WelcomeViewController.swift
@@ -14,9 +14,21 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+//        titleLabel.text = "⚡️FlashChat"
+        
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            print("-")
+            print(0.1 * charIndex)
+            print(letter)
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
     }
-    
-
 }
+
